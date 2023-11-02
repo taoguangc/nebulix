46e7dbdc8cea064d19a2fac6b662973850b8ee1d
taoguangc <taoguangc@gmail.com>
2023-11-02 09:41:01 +0800

    remove .env in .gitignore

--- src/util/translate.ts
+++ src/util/translate.ts
@@ -1,29 +1,31 @@
-
 interface Locale {
-    [key: string]: string;
+  [key: string]: string;
 }
-import nl from '@locales/nl.json';
-import en from '@locales/en.json';
-import de from '@locales/de.json';
-import es from '@locales/es.json';
-import hr from '@locales/hr.json';
+import nl from "@locales/nl.json";
+import en from "@locales/en.json";
+import de from "@locales/de.json";
+import es from "@locales/es.json";
+import hr from "@locales/hr.json";
+import zh from "@locales/zh.json";
+
 const lang = import.meta.env.WEBSITE_LANGUAGE;
 export const t = (field: string): string => {
-    const translations: Record<string, Locale>  = {
-        en: en,
-        nl: nl,
-        es: es,
-        de: de,
-        hr: hr,
-    };
+  const translations: Record<string, Locale> = {
+    en: en,
+    nl: nl,
+    es: es,
+    de: de,
+    hr: hr,
+    zh: zh,
+  };
 
-    if (translations[lang] && translations[lang][field]) {
-        return translations[lang][field];
-    }
+  if (translations[lang] && translations[lang][field]) {
+    return translations[lang][field];
+  }
 
-    if (translations['en'] && translations['en'][field]) {
-        return translations['en'][field];
-    }
+  if (translations["en"] && translations["en"][field]) {
+    return translations["en"][field];
+  }
 
-    return field;
+  return field;
 };
